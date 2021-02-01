a440e11747bd5b8f343971844900c6b7166916b4
Marvin Schopf <marvin@schopf.biz>
2021-02-01 23:30:26 +0100

    Add basic web server

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import http from "http";
+import { promises as fs } from "fs";
+import { resolve } from "path";
+
+const PORT: number = parseInt(process.env.PORT) || 3000;
+const STORAGE: string =
+	resolve(__dirname, "..", process.env.STORAGE) ||
+	resolve(__dirname, "..", "counter.txt");
+
+http
+	.createServer(
+		async (
+			req: http.IncomingMessage,
+			res: http.ServerResponse
+		): Promise<void> => {
+			res.writeHead(200, { "Content-Type": "text/plain" });
+			switch (req.url) {
+				case "/get":
+				case "/get/":
+					res.write(await fs.readFile(STORAGE));
+					break;
+				case "/update":
+				case "/update/":
+					const counter: number = parseInt(
+						(await fs.readFile(STORAGE)).toString("utf-8")
+					);
+					await fs.writeFile(STORAGE, (counter + 1).toString(), "utf-8");
+					res.write((counter + 1).toString());
+					break;
+			}
+			res.end();
+		}
+	)
+	.listen(PORT);
